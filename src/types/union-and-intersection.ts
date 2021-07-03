47365574737d1003256976fc6f784c6d1e7d5704
Sascha Homeier <shomeier@apollon.de>
2021-07-03 15:11:26 +0700

    Commented type behaviour

--- src/types/union-and-intersection.ts
+++ src/types/union-and-intersection.ts
@@ -8,10 +8,12 @@ interface Fish {
     swim(): void;
 }
 
+// on union types you can only invoke methods defined on ALL types
 function getSmallPet(pet: Fish | Bird) {
     pet.eat();
 }
 
+// on intersection types you can invoke all methods
 function getFlyingFish(pet: Fish & Bird) {
     pet.eat();
     pet.fly();
