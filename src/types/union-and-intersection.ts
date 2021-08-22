848c8cbeb8d34fc77671e5acf579acd604d89925
Sascha Homeier <shomeier@apollon.de>
2021-08-22 16:36:14 +0700

    Added example for union/intersection types

--- src/types/union-and-intersection.ts
+++ src/types/union-and-intersection.ts
@@ -19,3 +19,11 @@ function getFlyingFish(pet: Fish & Bird) {
     pet.fly();
     pet.swim();
 }
+
+// for union types the value must be either of type 'Fish', 'Bird' or of 'Fish & Bird'
+getSmallPet({ eat: () => console.log("eat"), fly: () => console.log("fly") });
+getSmallPet({ eat: () => console.log("eat"), swim: () => console.log("swim") });
+getSmallPet({ eat: () => console.log("eat"), fly: () => console.log("fly"), swim: () => console.log("swim") });
+
+// for intersection types the value must be of type 'Fish & Bird'
+getFlyingFish({ eat: () => console.log("eat"), fly: () => console.log("fly"), swim: () => console.log("swim") });
